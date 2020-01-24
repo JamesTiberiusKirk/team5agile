f267c65b1ae248a01143584fa821a44302804490
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-01-24 11:54:36 +0000

    Fix for absolute paths

--- Software_Code/certbot.sh
+++ Software_Code/certbot.sh
@@ -1,8 +1,8 @@
 docker run -it --rm \
--v ./docker-volumes/cert_bot/certs/:/etc/letsencrypt \
--v ./docker-volumes/cert_bot/certs_data/:/var/lib/letsencrypt \
--v "./docker-volumes/cert_bot/certbot_logs/:/var/log/letsencrypt" \
--v ./docker-volumes/cert_bot/certbot/root/:/root/ \
+-v /root/team5agile/Software_Code/docker-volumes/cert_bot/certs/:/etc/letsencrypt \
+-v /root/team5agile/Software_Code/docker-volumes/cert_bot/certs_data/:/var/lib/letsencrypt \
+-v "/root/team5agile/Software_Code/docker-volumes/cert_bot/certbot_logs/:/var/log/letsencrypt" \
+-v /root/team5agile/Software_Code/docker-volumes/cert_bot/certbot/root/:/root/ \
 certbot/certbot \
 certonly --manual --preferred-challenges dns \
 --server https://acme-v02.api.letsencrypt.org/directory \
