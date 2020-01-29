08d3af48819fb496cba2f9bfeb02320af5d9b08e
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-01-29 10:58:22 +0000

    Created test search for providers

--- Software_Code/backend/main.js
+++ Software_Code/backend/main.js
@@ -8,12 +8,20 @@ const app = express();
 
 db = new Db();
 
+
+// Helper functions
+function log(req, res) {
+  console.log(`[${req.method}] ${req.url}`);
+}
+
 // Middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id');
   res.header('Access-Control-Expose-Headers', 'x-access-token, x-refresh-token');
+
+  log(req, res);
 
   next();
 });
@@ -26,17 +34,21 @@ app.get('/', (req, res) => {
   console.log('[GET] /');
 });
 
-app.get('/providers/', (req, res) => {
-  /*let sql = `SELECT * FROM healthcare.provider WHERE;`
-  db.conn.query(sql, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-      return console.log(err);
-    }
-    //console.log(result);
-    res.status(200).send(result);
-  });*/
-  res.status(200).send("ok");
+app.get('/providers', (req, res) => {
+  let queryParams = req.query.search_query;
+  if (queryParams) {
+    let sql = `SELECT * FROM healthcare.provider WHERE provider_Name LIKE "%${queryParams}%";`
+
+    db.conn.query(sql, (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+        return console.log(`[GET] /providers error: ${err}`);
+      }
+      res.status(200).send(result);
+    });
+  } else {
+    res.status(405).send('Need to provide a seach query');
+  }
 });
 
 app.post('/provider/add', (req, res) => {
@@ -55,31 +67,23 @@ app.post('/provider/add', (req, res) => {
       provider_referral: bodyData.provider_referral
     };
 
-    /*sql = `CALL insertProvider( ${newPrv.provider_ID}, 
-                                ${newPrv.provider_Name}, 
-                                ${newPrv.provider_StreetAdd}, 
-                                ${newPrv.provider_City}, 
-                                ${newPrv.provider_State} 
-                                ${newPrv.provider_Zip}, 
-                                ${newPrv.provider_referral})`;*/
-
-/*
-    sql = `INSERT INTO 'provider' ('provider_ID','provider_Name', 
-            'provider_StreetAdd', 'provider_City', 'provider_State', 
-            'provider_ Zip', 'provider_referral') 
-          VALUES ( ${newPrv.provider_ID}, 
-            ${newPrv.provider_Name}, 
-            ${newPrv.provider_StreetAdd}, 
-            ${newPrv.provider_City}, 
-            ${newPrv.provider_State} 
-            ${newPrv.provider_Zip}, 
-            ${newPrv.provider_referral});`
-
-    db.conn.query(sql, (err, result) => {
-      if (err) return console.log(err);
-      console.log(result);
-    });
-    */
+    /*
+        sql = `INSERT INTO 'provider' ('provider_ID','provider_Name', 
+                'provider_StreetAdd', 'provider_City', 'provider_State', 
+                'provider_ Zip', 'provider_referral') 
+              VALUES ( ${newPrv.provider_ID}, 
+                ${newPrv.provider_Name}, 
+                ${newPrv.provider_StreetAdd}, 
+                ${newPrv.provider_City}, 
+                ${newPrv.provider_State} 
+                ${newPrv.provider_Zip}, 
+                ${newPrv.provider_referral});`
+    
+        db.conn.query(sql, (err, result) => {
+          if (err) return console.log(err);
+          console.log(result);
+        });
+        */
   } catch (e) {
     console.log(`[POST] ${req.url} error: ${e}`);
     return res.status(500).send(e.message);
@@ -95,7 +99,5 @@ var server = app.listen(3000, () => {
   var port = server.address().port;
   console.log('Example app listening at port %s', port);
 });
-
-
 
 module.exports = server;
