a8ebe99ae22e24750b918bfa412432cf70998a7a
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-01-31 13:05:42 +0000

    Pushing a quick fix

--- Software_Code/backend/db/db.js
+++ Software_Code/backend/db/db.js
@@ -7,7 +7,7 @@ class Db {
         this.conn;
         this.initConnection();
         this.handleConnection();
-        this.handleError();
+        // this.handleError();
     }
 
     initConnection() {
@@ -25,15 +25,15 @@ class Db {
         });
     }
 
-    handleError(){
-        this.conn.on('error',(err)=>{
-            if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-                this.initConnection();
-            } else {
-                throw err;
-            }
-        });
-    }
-}
+//     handleError(){
+//         this.conn.on('error',(err)=>{
+//             if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//                 this.initConnection();
+//             } else {
+//                 throw err;
+//             }
+//         });
+//     }
+// }
 
 module.exports = { Db };
