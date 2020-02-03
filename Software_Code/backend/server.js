151a0ac462ffc24736706444661265ff17839da8
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-03 15:55:45 +0000

    Added a routes index file

--- Software_Code/backend/server.js
+++ Software_Code/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+let { router, setDb } = require('./routes/index');
+
 // Helper functions
 function log(req, res) {
   console.log(`[${req.method}] ${req.url}`);
@@ -30,57 +32,9 @@ class Server {
 
   // Routes
   initRoutes() {
-    this.app.get('/', (req, res) => {
-      res.status(200).send('OK');
-    });
-
-    this.app.get('/zip2coords', (req, res) => {
-      let zip = req.query.zip;
-      if (zip) {
-        let sql = `SELECT * FROM healthcare.zip_coords WHERE zip_Code=${zip}`;
-        this.db.conn.query(sql, (err, result) => {
-          if (err) {
-            res.status(500).send(err);
-            return console.log(`[GET] /zip2coords error: ${err}`);
-          }
-          res.status(200).send(result);
-        });
-      } else {
-        res.status(400).send('Please provide a zip code');
-      }
-    });
-
-    this.app.get('/providers', (req, res) => {
-      let queryParams = req.query.search_query;
-      if (queryParams) {
-        let sql = `SELECT * FROM healthcare.provider WHERE provider_Name LIKE "%${queryParams}%" OR provider_ID="${queryParams}";`
-        this.db.conn.query(sql, (err, result) => {
-          if (err) {
-            res.status(500).send(err);
-            return console.log(`[GET] /providers error: ${err}`);
-          }
-          res.status(200).send(result);
-        });
-      } else {
-        res.status(405).send('Need to provide a seach query');
-      }
-    });
-
-    this.app.get('/procedures', (req, res) => {
-      let queryParams = req.query.search_query;
-      if (queryParams) {
-        let sql = `CALL sortRefineOptions("${queryParams}","${queryParams}","","","avg_Covered_Charges","ASC");`
-        this.db.conn.query(sql, (err, result) => {
-          if (err) {
-            res.status(500).send(err);
-            return console.log(`[GET] /procedures error: ${err}`);
-          }
-          res.status(200).send(result);
-        });
-      } else {
-        res.status(405).send('Need to provide a seach query');
-      }
-    });
+    setDb(this.db);
+    // db = this.db
+    this.app.use('/',router);
   }
 
   inirServer() {
