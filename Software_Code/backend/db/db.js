baac51462f4909c03edeb7e8617aadf89186a377
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-03 15:39:02 +0000

    Refacored the db conn

--- Software_Code/backend/db/db.js
+++ Software_Code/backend/db/db.js
@@ -4,36 +4,55 @@ const db_config = require('../config/db.config');
 
 class Db {
     constructor() {
-        this.conn;
-        this.initConnection();
-        this.handleConnection();
-        // this.handleError();
     }
 
     initConnection() {
-        this.conn = mysql.createConnection(db_config);
-    }
-
-    handleConnection(){
-        this.conn.connect((err) => {
-            if (err) {
-                console.error('MySql error connecting: ' + err.stack);
-                return;
-            }
-
-            console.log('MySql connected...');
+        return new Promise((reject, resolve) => {
+            this.conn = mysql.createConnection(db_config);
+            this.handleConnection()
+                .then(() => {
+                    // this.handleError();
+                    resolve();
+                })
+                .catch((err) => {
+                    reject(err);
+                });
         });
     }
 
-//     handleError(){
-//         this.conn.on('error',(err)=>{
-//             if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-//                 this.initConnection();
-//             } else {
-//                 throw err;
-//             }
-//         });
-//     }
+    handleConnection() {
+        return new Promise((reject, resolve) => {
+            this.conn.connect((err) => {
+                if (err) {
+                    console.error('MySql error connecting: ' + err.stack);
+                    reject();
+                    return;
+                }
+                resolve();
+                console.log('MySql connected...');
+            });
+
+        })
+    }
+
+    closeConn() {
+        return new Promise((resolve) => {
+            this.conn.end(()=>{
+                console.log('MySql conn closed.')
+                resolve();
+            });
+        })
+    }
+
+    //     handleError(){
+    //         this.conn.on('error',(err)=>{
+    //             if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+    //                 this.initConnection();
+    //             } else {
+    //                 throw err;
+    //             }
+    //         });
+    //     }
 }
 
 module.exports = { Db };
