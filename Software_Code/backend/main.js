6a8bef67036c1f6de4ad18714827c44409f3c2b7
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-03 15:39:20 +0000

    Fixed db init

--- Software_Code/backend/main.js
+++ Software_Code/backend/main.js
@@ -2,6 +2,10 @@
 const { Db } = require('./db/db');
 const { Server } = require('./server');
 
-let db = new Db().then(()=>{
-  let server = new Server(db);
-});
+let db = new Db();
+db.initConnection()
+  .then(() => {
+    new Server(db);
+  }).catch((err) => {
+    console.error(err);
+  });
