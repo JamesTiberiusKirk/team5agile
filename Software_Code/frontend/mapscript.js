0f9e6b4d330d5b1519d08f5580423850bf737239
dompio <d.piosik@dundee.ac.uk>
2020-02-06 17:10:49 +0000

    Update mapscript.js
    close popup at click outside the popup

--- Software_Code/frontend/mapscript.js
+++ Software_Code/frontend/mapscript.js
@@ -333,7 +333,7 @@ function closeNav() {
       function createPopUp(currentFeature) {
         var popUps = document.getElementsByClassName('mapboxgl-popup');
         if (popUps[0]) popUps[0].remove();
-        var popup = new mapboxgl.Popup({closeOnClick: false})
+        var popup = new mapboxgl.Popup({closeOnClick: true})
           .setLngLat(currentFeature.geometry.coordinates)
           .setHTML('<h3>' + currentFeature.properties.provider_Name +'</h3>' +
             '<h4>' + '$' + currentFeature.properties.avg_Medicare_Payments + '</h4>')
